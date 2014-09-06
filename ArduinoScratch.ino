c2d06aa8955ff8bbe56450a3162017f6590b2e15
piif <christian_lefebvre@laposte.net>
2014-09-06 17:29:18 +0200

    added input button

--- ArduinoScratch.ino
+++ ArduinoScratch.ino
@@ -1,19 +1,38 @@
 #include <Arduino.h>
+#ifndef DEFAULT_BAUDRATE
+	#define DEFAULT_BAUDRATE 115200
+#endif
+
+#define LED 13
+#define BUTTON 12
 
 bool ledState = LOW;
-bool buttonState = LOW;
+bool buttonState = HIGH;
+bool buttonLastChange = 0;
 
 void setup(void) {
-	pinMode(13, OUTPUT);
-	digitalWrite(13, LOW);
+	pinMode(LED, OUTPUT);
+	digitalWrite(LED, LOW);
 
 	// TODO : bind raising interrupt on this digital input
-	pinMode(12, INPUT);
+	pinMode(BUTTON, INPUT_PULLUP);
 
-	Serial.begin(115200);
+	Serial.begin(DEFAULT_BAUDRATE);
 }
 
 void loop() {
+	// test button
+	bool b = digitalRead(BUTTON);
+	if (b != buttonState && millis() - buttonLastChange > 100) {
+		//Serial.println(millis() - buttonLastChange);
+		buttonLastChange = millis();
+		buttonState = b;
+		if (buttonState == LOW) {
+			Serial.println('!');
+		}
+	}
+
+	// test input
 	int input = Serial.read();
 	if (input == '0') {
 		ledState = LOW;
@@ -22,5 +41,7 @@ void loop() {
 	} else if (input == '?') {
 		Serial.println(ledState ? "1" : "0");
 	}
+
+	// update LED
 	digitalWrite(13, ledState);
 }
